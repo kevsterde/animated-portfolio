9ebfad2e6faac6f7d4ac9a0c188a5bb08724b537
Kevin Webster Revalde <kevinwebsterr2016@gmail.com>
2024-02-20 23:24:01 +0800

    update 2024

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,92 +1,103 @@
-import React from 'react'
-import './hero.scss'
-import { motion } from 'framer-motion'
+import React from "react";
+import "./hero.scss";
+import { motion } from "framer-motion";
 
 const textVariants = {
-    initial: {
-        x: - 500,
-        opacity: 0,
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
     },
-    animate: {
-        x: 0,
-        opacity: 1,
-        transition: {
-            duration: 1,
-            staggerChildren: 0.1,
-        }
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
     },
-    scrollButton: {
-        opacity: 0,
-        y: 10,
-        transition: {
-            duration: 2,
-            repeat: Infinity,
-        }
-    }
-}
+  },
+};
 
 const slidingText = {
-    initial: {
-        x: 0,
-
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-150%",
+    transition: {
+      duration: 20,
+      repeat: Infinity,
+      repeatType: "mirror",
     },
-    animate: {
-        x: "-150%",
-        transition: {
-            duration: 20,
-            repeat: Infinity,
-            repeatType: "mirror"
-        }
-    }
-
-
-}
-
-
-
+  },
+};
 
 function Hero() {
-    return (
-        <div className='hero'>
-            <div className="wrapper">
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>KEVIN REVALDE</motion.h2>
+          <motion.h1 variants={textVariants}>WEB DEVELOPER</motion.h1>
 
-                <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
-                    <motion.h2 variants={textVariants}>KEVIN REVALDE</motion.h2>
-                    <motion.h1 variants={textVariants}>FRONT-END DEVELOPER</motion.h1>
-
-                    <motion.div variants={textVariants} className="button">
-                        <a href="#">My Resume</a>
-                        <a href="#Contact" class="contact_btn">Contact Me</a>
-                    </motion.div>
-                    <a href="#"><motion.img variants={textVariants} animate="scrollButton" src="/scroll.png" alt="" /></a>
-                </motion.div>
-            </div>
-            <motion.div className="slidingText" variants={slidingText} initial="initial" animate="animate">
-                <img src="/html.png" alt="" />
-                <img src="/css.png" alt="" />
-                <img src="/js.png" alt="" />
-                <img src="/react.png" alt="" />
-                <img src="/wordpress.png" alt="" />
-
-
-            </motion.div>
-            <motion.div className="imageContainer" initial={{
-                y: "100%"
-            }}
-
-                animate={{
-                    y: "0%", transition: {
-                        duration: .8,
-
-                    }
-
-                }}
-
-            >
-                <img src="/kwr.png" alt="kevin revalde" />
-            </motion.div>
-        </div >
-    )
+          <motion.div variants={textVariants} className="button">
+            <a href="/pdf/RevaldeCV2024.pdf" download>
+              My Resume
+            </a>
+            <a href="#Contact" className="contact_btn">
+              Contact Me
+            </a>
+          </motion.div>
+          <a href="#">
+            <motion.img
+              variants={textVariants}
+              animate="scrollButton"
+              src="/scroll.png"
+              alt=""
+            />
+          </a>
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingText"
+        variants={slidingText}
+        initial="initial"
+        animate="animate"
+      >
+        <img src="/html.png" alt="" />
+        <img src="/css.png" alt="" />
+        <img src="/js.png" alt="" />
+        <img src="/react.png" alt="" />
+        <img src="/wordpress.png" alt="" />
+      </motion.div>
+      <motion.div
+        className="imageContainer"
+        initial={{
+          y: "100%",
+        }}
+        animate={{
+          y: "0%",
+          transition: {
+            duration: 0.8,
+          },
+        }}
+      >
+        <img src="/kwr.png" alt="kevin revalde" />
+      </motion.div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,90 +1,102 @@
-import React, { useRef } from 'react'
-import './portfolio.scss'
+import React, { useRef } from "react";
+import "./portfolio.scss";
 
-import { motion, useScroll, useSpring, useTransform } from "framer-motion"
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
-    {
-        id: 1,
-        title: "KWR Font",
-        img: "./kwrfont.png",
-        desc: "KWRFONT is a website where you can store, access, and utilize style fonts css with just copy & paste to your stylesheet.",
-        link: "https://kevsterde.great-site.net/kwrfont/",
-    },
-    {
-        id: 2,
-        title: " KWR Generator",
-        img: "/kwrgenerator.webp",
-        desc: "I created this kwrgenerator to develop the website effeciently by inputting only the title, paragraphs, alt value then returning a html structured and css style.",
-        link: "https://kevsterde.github.io/kwrgenerator/"
-    },
-    {
-        id: 3,
-        title: "PawTrain",
-        img: "/pawtrain.webp",
-        desc: "Dog Website using Tailwind CSS",
-        link: "https://kevsterde.github.io/tailwind1/"
-    },
-
-]
-
+  {
+    id: 1,
+    title: "WFH-JOB FINDER",
+    img: "./wfh-jobfinder.png",
+    desc: "WFH-JOB FINDER is a job posting website built using Bubble.io",
+    link: "https://wfh-jobfinder.bubbleapps.io/version-test",
+  },
+  {
+    id: 2,
+    title: "ANG SHARERAP",
+    img: "./angsharerap.png",
+    desc: "Ang Sharerap is a recipe sharing platform website so that user can share their delicious recipe. Website is developed using Bubble.io",
+    link: "https://ang-sharerap.bubbleapps.io/version-test/",
+  },
+  {
+    id: 3,
+    title: "RETSBEW SHOP",
+    img: "./retsbew.png",
+    desc: "RETSBEW SHOP is a online selling platform where you can post and sell your product. Website is developed using Bubble.io",
+    link: "https://retsbew.bubbleapps.io/version-test",
+  },
+  {
+    id: 4,
+    title: "KWR Font",
+    img: "./kwrfont.png",
+    desc: "KWRFONT is a website where you can store, access, and utilize style fonts css with just copy & paste to your stylesheet.",
+    link: "https://kevsterde.great-site.net/kwrfont/",
+  },
+  {
+    id: 5,
+    title: " KWR Generator",
+    img: "/kwrgenerator.webp",
+    desc: "I created this kwrgenerator to develop the website effeciently by inputting only the title, paragraphs, alt value then returning a html structured and css style.",
+    link: "https://kevsterde.github.io/kwrgenerator/",
+  },
+  {
+    id: 6,
+    title: "PawTrain",
+    img: "/pawtrain.webp",
+    desc: "Dog Website using Tailwind CSS",
+    link: "https://kevsterde.github.io/tailwind1/",
+  },
+];
 
 const Single = ({ item }) => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["start start", "end start"]
+  });
 
-    const ref = useRef();
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        // offset: ["start start", "end start"]
-    })
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
-    const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
-
-    return (
-
-
-        <section >
-            <div className="wrapper">
-                <div className="container">
-
-
-                    <motion.img ref={ref} src={item.img} alt={item.title} />
-                    <motion.div style={{ y }} className="textContainer" >
-                        <h2>{item.title}</h2>
-                        <p>{item.desc}</p>
-                        <a href={item.link} target='_blank'>See Demo</a>
-                    </motion.div>
-                </div>
-            </div>
-        </section>
-    )
-
-}
-
+  return (
+    <section>
+      <div className="wrapper">
+        <div className="container">
+          <motion.img ref={ref} src={item.img} alt={item.title} />
+          <motion.div style={{ y }} className="textContainer">
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <a href={item.link} target="_blank" rel="noreferrer">
+              See Demo
+            </a>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 const Portfolio = () => {
-    const ref = useRef()
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ["end end", "start start"]
-    })
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
 
-    const scaleX = useSpring(scrollYProgress, {
-        stiffness: 100,
-        damping: 30,
-    })
-    return (
-        <div className='portfolio' ref={ref}>
-            <div className="progress">
-                <h1>Personal Works</h1>
-                <motion.div style={{ scaleX }} className="progressBar"></motion.div>
-            </div>
-            {items.map(item => (
-                <Single item={item} key={item.id} />
-            ))}
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Personal Works</h1>
+        <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
 
-
-        </div>
-    )
-}
-
-export default Portfolio
+export default Portfolio;
